f5a38861a206c40b1e47a298570f8ad46b17cc9f
[TSKY.qinglong.l] <[qinglongl@travelsky.com]>
2017-09-12 10:23:21 +0800

    modify670

--- AlgorithmTest/src/array/easy/MaximumSwap670.java
+++ AlgorithmTest/src/array/easy/MaximumSwap670.java
@@ -3,22 +3,23 @@ package array.easy;
 
 public class MaximumSwap670 {
 	public static void main(String[] args) {
-		System.out.println(maximumSwap1(99736));
+		System.out.println(maximumSwap(99736));
 	}
 
 	public static int maximumSwap(int num) {
 		char[] digits = Integer.toString(num).toCharArray();
-
 		int[] buckets = new int[10];
 		for (int i = 0; i < digits.length; i++) {
 			buckets[digits[i] - '0'] = i;
 		}
-
+		//当它走到第三位的时候，发现先出现了  4  ，那么第四位的数字在第三位后面，也就是说第四位的数字比第三位数字大，所以它俩需要交换。
 		for (int i = 0; i < digits.length; i++) {
+			//值大于当前下标对应的char值 
 			for (int k = 9; k > digits[i] - '0'; k--) {
+				//对应k值的下标大于当前i的下标，说明此值比当前下标对应的值大，所以需要交换
 				if (buckets[k] > i) {
 					char tmp = digits[i];
-					digits[i] = digits[buckets[k]];
+				 	digits[i] = digits[buckets[k]];
 					digits[buckets[k]] = tmp;
 					return Integer.valueOf(new String(digits));
 				}
