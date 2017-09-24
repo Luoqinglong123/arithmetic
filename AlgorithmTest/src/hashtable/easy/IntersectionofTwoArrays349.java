92f4b512d8391ce5b25c8961ca0199087bc29996
[TSKY.qinglong.l] <[qinglongl@travelsky.com]>
2017-09-24 18:17:48 +0800

    2017/9/24/sun

---
+++ AlgorithmTest/src/hashtable/easy/IntersectionofTwoArrays349.java
@@ -0,0 +1,35 @@
+package hashtable.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionofTwoArrays349 {
+
+	public static void main(String[] args) {
+		int[] nums2 = { 2, 2, 3, 4 };
+		int[] nums1 = { 1, 2, 3, 2, 1 };
+		int[] intersection = intersection(nums1, nums2);
+		for (int i : intersection) {
+			System.out.print(i + "\t");
+		}
+	}
+
+	public static int[] intersection(int[] nums1, int[] nums2) {
+		Set<Integer> set = new HashSet();
+		Set<Integer> resSet = new HashSet();
+		for (int i = 0; i < nums1.length; i++) {
+			set.add(nums1[i]);
+		}
+		for (int i = 0; i < nums2.length; i++) {
+			if (set.contains(nums2[i]))
+				resSet.add(nums2[i]);
+		}
+		int[] res = new int[resSet.size()];
+		int n = 0;
+		for (int i : resSet) {
+			res[n++] = i;
+		}
+		return res;
+
+	}
+}
---
+++ AlgorithmTest/src/hashtable/easy/WordPattern290.java
@@ -0,0 +1,22 @@
+package hashtable.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordPattern290 {
+
+
+	public static boolean wordPattern1(String pattern, String str) {
+		String[] split = str.split(" ");
+		if (split.length != pattern.length())
+			return false;
+		Map map = new HashMap();
+		for (Integer i = 0; i < split.length; i++) {
+			// 上次放的值是上一个下标为i的值res，这一次返回的值就是res (key不同，但是value相同)
+			// map.put()返回上一次放入的value
+			if (map.put(pattern.charAt(i), i) != map.put(split[i], i))
+				return false;
+		}
+		return true;
+	}
+}
