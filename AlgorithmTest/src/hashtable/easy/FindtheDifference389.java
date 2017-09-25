a26a95fca5166e0e4db35c502a0676187ed2022d
[TSKY.hui.wang] <[wh@travelsky.com]>
2017-09-25 21:26:36 +0800

    2017/9/25/mon

---
+++ AlgorithmTest/src/hashtable/easy/FindtheDifference389.java
@@ -0,0 +1,22 @@
+package hashtable.easy;
+
+public class FindtheDifference389 {
+	public static void main(String[] args) {
+		String s = "abcd";
+		String t = "abcde";
+		System.out.println(findTheDifference(s, t));
+	}
+
+	public static char findTheDifference(String s, String t) {
+		int res = 0, tres = 0;
+		for (int i = 0; i < s.length(); i++) {
+			res += (int)s.charAt(i);
+		}
+		for (int i = 0; i < t.length(); i++) {
+			tres += (int)t.charAt(i);
+		}
+
+		return (char)(tres-res);
+
+	}
+}
