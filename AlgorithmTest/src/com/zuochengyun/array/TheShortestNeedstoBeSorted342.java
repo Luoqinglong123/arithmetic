9902d84d31f0272a5314b21891c2e83ca044c10e
754711156@qq.com <754711156@qq.com>
2017-10-11 23:42:59 +0800

    左神342

---
+++ AlgorithmTest/src/com/zuochengyun/array/TheShortestNeedstoBeSorted342.java
@@ -0,0 +1,35 @@
+package com.zuochengyun.array;
+
+public class TheShortestNeedstoBeSorted342 {
+
+	public static void main(String[] args) {
+		int[] arr={1,5,3,4,2,6,7};
+		System.out.println(getMinLength(arr));
+	}
+	public static int getMinLength(int[] arr) {
+		if (arr == null || arr.length < 2)
+			return 0;
+		int min = arr[arr.length - 1];
+		int noMinIndex = -1;
+		for (int i = arr.length - 2; i != -1; i--) {
+			if (arr[i] > min) {
+				noMinIndex = i;
+			} else {
+				min = Math.min(min, arr[i]);
+			}
+		}
+		if (noMinIndex == -1)
+			return 0;
+		int max = arr[0];
+		int noMaxIndex = -1;
+		for (int i = 1; i != arr.length; i++) {
+			if (arr[i] < max) {
+				noMaxIndex = i;
+			} else {
+				max = Math.max(max, arr[i]);
+			}
+		}
+		return noMaxIndex - noMinIndex + 1;
+
+	}
+}
