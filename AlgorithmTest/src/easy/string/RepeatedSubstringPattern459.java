0d61b1ac7106e8a2d68f21c7b187522c0799997e
[TSKY.qinglong.l] <[qinglongl@travelsky.com]>
2017-10-20 18:52:41 +0800

    检查一个字符串是否由多个子字符串构成  2017/10/20 Fri

---
+++ AlgorithmTest/src/easy/string/RepeatedSubstringPattern459.java
@@ -0,0 +1,25 @@
+package easy.string;
+
+public class RepeatedSubstringPattern459 {
+	
+	public static void main(String[] args) {
+		boolean repeatedSubstringPattern = repeatedSubstringPattern("abcabcabcabc");
+	}
+	public  static boolean repeatedSubstringPattern(String s) {
+		int l = s.length();
+		for (int i = l / 2; i >= 1; i--) {
+			if (l % i == 0) {
+				int m = l / i;
+				String substring = s.substring(0, i);
+				StringBuilder sb = new StringBuilder();
+				for (int j = 0; j < m; j++) {
+					
+					sb.append(substring);
+				}
+				
+				if(sb.toString().equals(s)) return true;
+			}
+		}
+		return false;
+	}
+}
