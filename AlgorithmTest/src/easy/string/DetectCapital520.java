2f41738fa46d64d4642991ddb36c0667c67d6909
754711156@qq.com <754711156@qq.com>
2017-10-22 14:53:53 +0800

    优化

--- AlgorithmTest/src/easy/string/DetectCapital520.java
+++ AlgorithmTest/src/easy/string/DetectCapital520.java
@@ -2,33 +2,44 @@ package easy.string;
 
 public class DetectCapital520 {
 	public static void main(String[] args) {
-		String word="G";
+		String word = "G";
 		System.out.println(detectCapitalUse(word));
 	}
+
 	public static boolean detectCapitalUse(String word) {
-		if(word.length()==1) return true;
-		if (word == null || word.length() <1)
+		if (word.length() == 1)
+			return true;
+		if (word == null || word.length() < 1)
 			return false;
-		int first=word.charAt(0)-'a';
-		if(first>=0){
+		int first = word.charAt(0) - 'a';
+		if (first >= 0) {
 			for (int i = 1; i < word.length(); i++) {
-				if(word.charAt(i)-'a'<0) 
+				if (word.charAt(i) - 'a' < 0)
 					return false;
 			}
-		}else{
-			int second=word.charAt(1)-'a';
-			if(second>=0){
+		} else {
+			int second = word.charAt(1) - 'a';
+			if (second >= 0) {
 				for (int i = 2; i < word.length(); i++) {
-					if(word.charAt(i)-'a'<0) 
+					if (word.charAt(i) - 'a' < 0)
 						return false;
 				}
-			}else{
+			} else {
 				for (int i = 2; i < word.length(); i++) {
-					if(word.charAt(i)-'a'>=0) 
+					if (word.charAt(i) - 'a' >= 0)
 						return false;
 				}
 			}
 		}
 		return true;
+	}
+
+	public static boolean detectCapitalUse1(String word) {
+		int i = 0 ; 
+		for (int j = 0; j < word.length(); j++) {
+			if('Z'-word.charAt(j)>=0)
+				i++;
+		}
+		return i==0||i==word.length()||'Z'-word.charAt(0)>=0&&i==1;
 	}
 }
